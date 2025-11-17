236d961c2de8862006c18f7bcd4ec6b9d1064442
Stanisław Bożych <stachubozych@gmail.com>
2025-11-17 18:23:43 +0100

    feat: linter and formatter setup with husky

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  singleQuote: true,       // use single quotes
+  bracketSpacing: true,    // { a: 1 } insted {a:1}
+  trailingComma: 'all',    // commas at end when possible
+  arrowParens: 'always',   // (x) => insted x =>
+  printWidth: 100,         // max line length before break
+  useTabs: false,          // spaces, not tabs
+  tabWidth: 2,             // tab witdth in spaces
+  semi: true               // semicolons required
+};
+
