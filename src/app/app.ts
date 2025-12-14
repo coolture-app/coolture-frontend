93f60ac2e01268a648fbafceaa4b1bccd437f323
Stanisław Bożych <stachubozych@gmail.com>
2025-12-14 15:05:24 +0100

    feat: added localization

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,26 @@
-import { Component, signal } from '@angular/core';
+import { Component, signal, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Navbar } from './core/components/navbar/navbar';
+import { Footer } from './core/components/footer/footer';
+import { TranslateService } from '@ngx-translate/core';
+import translationEN from '../../public/i18n/en.json';
+import translationPL from '../../public/i18n/pl.json';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Navbar, Footer],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
 export class App {
+  private translate = inject(TranslateService);
   protected readonly title = signal('frontend');
+
+  constructor() {
+    this.translate.addLangs(['pl', 'en']);
+    this.translate.setTranslation('en', translationEN);
+    this.translate.setTranslation('pl', translationPL);
+    this.translate.setFallbackLang('pl');
+    this.translate.use('pl');
+  }
 }
--- src/app/core/components/navbar/navbar.ts
+++ src/app/core/components/navbar/navbar.ts
@@ -1,9 +1,23 @@
 import { Component, inject } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { map } from 'rxjs';
+import { TranslateDirective, TranslatePipe } from '@ngx-translate/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [TranslateDirective, TranslatePipe],
   templateUrl: './navbar.html',
   styleUrl: './navbar.scss',
 })
-export class Navbar {}
+export class Navbar {
+  private translate = inject(TranslateService);
+  currentLang = toSignal(this.translate.onLangChange.pipe(map((event) => event.lang)), {
+    initialValue: this.translate.getCurrentLang(),
+  });
+
+  changeLanguage(): void {
+    const newLang = this.currentLang() === 'pl' ? 'en' : 'pl';
+    this.translate.use(newLang);
+  }
+}
