3303dc4e7b0d29aa37b32403e819738adcf5a82b
Stanisław Bożych <stachubozych@gmail.com>
2025-12-14 14:58:15 +0100

    fix: fixed eslint configuration

--- eslint.config.js
+++ eslint.config.js
@@ -1,13 +1,13 @@
-;// @ts-check
-const eslint = require("@eslint/js");
-const tseslint = require("typescript-eslint");
-const angular = require("angular-eslint");
-const prettierConfig = require("eslint-config-prettier");
-const prettierPlugin = require("eslint-plugin-prettier");
+// @ts-check
+const eslint = require('@eslint/js');
+const tseslint = require('typescript-eslint');
+const angular = require('angular-eslint');
+const prettierConfig = require('eslint-config-prettier');
+const prettierPlugin = require('eslint-plugin-prettier');
 
 module.exports = tseslint.config(
   {
-    files: ["**/*.ts"],
+    files: ['**/*.ts'],
     extends: [
       eslint.configs.recommended,
       ...tseslint.configs.recommended,
@@ -20,37 +20,34 @@ module.exports = tseslint.config(
     },
     processor: angular.processInlineTemplates,
     rules: {
-      "@angular-eslint/directive-selector": [
-        "error",
+      '@angular-eslint/directive-selector': [
+        'error',
         {
-          type: "attribute",
-          prefix: "app",
-          style: "camelCase",
+          type: 'attribute',
+          prefix: 'app',
+          style: 'camelCase',
         },
       ],
-      "@angular-eslint/component-selector": [
-        "error",
+      '@angular-eslint/component-selector': [
+        'error',
         {
-          type: "element",
-          prefix: "app",
-          style: "kebab-case",
+          type: 'element',
+          prefix: 'app',
+          style: 'kebab-case',
         },
       ],
-      "space-in-parens": ["error", "never"],
-      "space-infix-ops": "error",
-      "space-before-blocks": ["error", "always"],
-      "space-before-function-paren": ["error", "always"],
-      "array-bracket-spacing": ["error", "never"],
+      'space-in-parens': ['error', 'never'],
+      'space-infix-ops': 'error',
+      'space-before-blocks': ['error', 'always'],
+      'space-before-function-paren': 'off',
+      'array-bracket-spacing': ['error', 'never'],
       // Prettier jako reguła
-      "prettier/prettier": ["error", require('./.prettierrc.js') || {}]
+      'prettier/prettier': ['error', require('./.prettierrc.js') || {}],
     },
   },
   {
-    files: ["**/*.html"],
-    extends: [
-      ...angular.configs.templateRecommended,
-      ...angular.configs.templateAccessibility,
-    ],
+    files: ['**/*.html'],
+    extends: [...angular.configs.templateRecommended, ...angular.configs.templateAccessibility],
     rules: {},
-  }
+  },
 );
