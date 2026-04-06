2c21ad2b313c92f582324ebead3b9a746cd725d4
jkalinowsky <j.kalinowskyy@gmail.com>
2026-04-06 21:08:55 +0200

    Refactor auth guard to use ApiUrlService for redirect URL

--- src/app/core/auth/auth.guard.ts
+++ src/app/core/auth/auth.guard.ts
@@ -1,11 +1,13 @@
 import { inject, PLATFORM_ID } from '@angular/core';
 import { CanActivateFn } from '@angular/router';
 import { AuthService } from '../services/auth/auth.service';
+import { ApiUrlService } from '../services/api-url.service';
 import { isPlatformBrowser } from '@angular/common';
 import { map } from 'rxjs';
 
 export const authGuard: CanActivateFn = () => {
   const authService = inject(AuthService);
+  const apiUrl = inject(ApiUrlService);
   const platformId = inject(PLATFORM_ID);
 
   return authService.checkSession().pipe(
@@ -14,8 +16,8 @@ export const authGuard: CanActivateFn = () => {
         return true;
       }
 
-      if (isPlatformBrowser(platformId)) {
-        window.location.href = 'http://localhost:8080/oauth2/authorization/keycloak';
+      if (isPlatformBrowser(platformId as object)) {
+        window.location.href = apiUrl.url.replace('/api', '') + '/oauth2/authorization/keycloak';
       } else {
         console.log('SSR: User not logged in, allowing render for client-side redirect.');
         return true;
