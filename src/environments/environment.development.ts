133bda5262d1cf4c0dac7ceded6135b5a6027b20
staa <stachubozych@gmail.com>
2026-04-24 19:45:42 +0200

    Changed env to match port correctly

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiUrlServer: 'http://localhost:8090/api',
-  apiUrlBrowser: 'http://localhost:8090/api',
+  apiUrlServer: 'http://localhost:8080/api',
+  apiUrlBrowser: 'http://localhost:8080/api',
 };
