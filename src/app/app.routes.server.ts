5df458ad1a77503b2a3dbd2a5e260f3747d41dff
staa <stachubozych@gmail.com>
2026-04-28 22:49:58 +0200

    fixed prerendering error

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,6 +2,14 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'u/:username',
+    renderMode: RenderMode.Server,
+  },
+  {
+    path: 'editPost/:id',
+    renderMode: RenderMode.Client,
+  },
+  {
     path: 'post/:id',
     renderMode: RenderMode.Server,
   },
