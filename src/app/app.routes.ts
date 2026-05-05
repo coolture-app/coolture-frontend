e62e39599a08d1f3d9f7e0209e8c02cf9b8c7fe0
Stanisław Bożych <stachubozych@gmail.com>
2026-05-05 19:59:16 +0200

    remove api-tests from router and fixed minor bugs

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,7 +6,6 @@ import { inject } from '@angular/core';
 import { NotFoundView } from './features/not-found-view/not-found-view';
 import { AddPostView } from './features/add-post-view/add-post-view';
 import { DiscoverView } from './features/discover-view/discover-view';
-import { ApiTestView } from './features/api-test-view/api-test-view';
 import { UserProfileView } from './features/my-profile-view/my-profile-view';
 import { authGuard } from './core/auth/auth.guard';
 import { EditPostView } from './features/edit-post-view/edit-post-view';
@@ -88,12 +87,6 @@ export const routes: Routes = [
       const translate = inject(TranslateService);
       return translate.get('MY_EVENTS.title');
     },
-  },
-  {
-    path: 'api-test',
-    component: ApiTestView,
-    canActivate: [authGuard],
-    title: 'API Test Runner',
   },
   // ** WILDCARD must be on the bottom of the router!
   {
